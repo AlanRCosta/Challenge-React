0da492271c1695f0cb4cab5deb461c876f0d163b
Alan Costa <alanrodrigues.costa@outlook.com>
2022-08-07 00:26:43 -0300

    Implementation of header features

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { Container, SearchForm } from "./styles";
 
 const Header: React.FC = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    navigate("/" + search.trim());
+  }
+
   return (
     <Container>
-      <SearchForm>
-        <input placeholder="Enter Username..." />
+      <SearchForm onSubmit={handleSubmit}>
+        <input
+          placeholder="Enter Username..."
+          value={search}
+          onChange={(e) => setSearch(e.currentTarget.value)}
+        />
       </SearchForm>
     </Container>
   );
